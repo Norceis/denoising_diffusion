b51d31bb4d9f95ca835dfa44fdde4af9867c4c85
Norceis <cubix77@gmail.com>
2023-11-23 13:07:52 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A short implementation of denoising diffusion probabilistic models article',
+    author='Jakub Kowalczewski',
+    license='MIT',
+)
